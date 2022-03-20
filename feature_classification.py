eed261dd30645ee3876c30d03b7c91641b52a156
Matt Collins <mc12338@my.bristol.ac.uk>
2022-03-20 08:58:25 +0000

    created py file and demo file

--- feature_classification.py
+++ feature_classification.py
@@ -9,12 +9,14 @@ from pprint import pprint
 pd.options.mode.chained_assignment = None  # default='warn'
 np.set_printoptions(precision=3)
 
+
+
 # Use SequenceMatcher function to calculate string similarity
 def similar(a, b):
     return SequenceMatcher(None, a, b).ratio()
 
 
-# Create correlecation matrix figure
+# Create correlation matrix figure
 def correlation_matrix(titles):
     df_list = []
     for i in titles:
@@ -33,7 +35,7 @@ def correlation_matrix(titles):
 
 # Apply grouping and return "stripped down" version of titles using the common values identified in the similarity check
 def group_similar(df,percent):
-    s = np.where(df.gt(percent,0), [i for i in df.columns], '')
+    s = np.where(df.gt(percent,0), [i for i in df.columns], '') # Note this doesn't yet work with pyspark.pandas!
     similar_lists = pd.Series([list(filter(None, i)) for i in s])
     
     groups = []
@@ -67,7 +69,6 @@ def group_similar(df,percent):
     titles_stripped = list(set(titles_stripped))    
     return titles_stripped
 
-
 # Evaluate the independence between each of the stripped titles.
 def independence_metric(titles_stripped):
     independence = []
@@ -77,12 +78,15 @@ def independence_metric(titles_stripped):
         for j in other_titles:
             if i not in j and len(i) > 1: 
                 independent += 1
-        val = round(independent/len(other_titles),2)
+        if len(other_titles) == 0:
+            val = 0
+        else:
+            val = round(independent/len(other_titles),2)
         independence.append(val)
     mean_independence = np.mean(independence)
     return mean_independence
 
-    
+
 # Main function to bring everything together
 def full(titles, percent=0.8,levels=1):
     features = [len(titles)]
@@ -98,11 +102,46 @@ def full(titles, percent=0.8,levels=1):
     return titles_stripped, features, fig, mean_independence
 
 
-def independence_graph(x, y):
-    fig = px.line(
-        x=x,
-        y=y,
-        labels=dict(x="Similarity (%)", y="Mean independence of values")
-        )
-    fig.update_layout(xaxis = dict(dtick = 1))
+# Test different similarities
+def test_thresholds(titles, thresholds):
+    #thresholds = list(range(1,10))
+    independences = []
+    for i in thresholds:
+        print('Similarity: {}%'.format(i * 10))
+        titles_stripped, features, fig, mean_independence = full(titles, 0.1 * i, 1)
+        print(features)
+        print('Mean independence: {}'.format(round(mean_independence,2)))
+        independences.append(round(mean_independence,2))
+        print()
     fig.show()
+    #fig.write_html("path/to/file.html")
+    return independences
+    
+def plot_thresholds(thresholds, independences):
+    thresholds_percent = [i * 10 for i in thresholds]
+    independences_percent = [i * 100 for i in independences]
+    fig = px.line(
+        x=thresholds_percent,
+        y=independences_percent,
+        labels=dict(x="Similarity (%)", y="Mean independence of values (%)"))
+    fig.show()
+    
+    
+# Substitute the stripped down titles into the DataFrame.
+def substitute_to_df(df, column, stripped):
+    list_1 = []
+    for i in df[column]:
+        list_2 = []
+        for j in stripped:
+            if j in i.lower():
+                list_2.append(j)
+            else:
+                pass
+        if list_2:
+            list_1.append(list_2[0])
+        else:
+            list_1.append(i)
+    col_stripped = column + '_stripped'
+    df[col_stripped] = list_1
+    return df
+
